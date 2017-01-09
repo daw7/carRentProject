2f5eea5dcc43d339ca9a398ed3b79d18d32ee466
Dawid <vdx@onet.pl>
2017-01-09 22:06:21 +0100

    First commit

---
+++ carRentProject/Controllers/DataController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using carRentProject.Models;
+
+namespace carRentProject.Controllers
+{
+    public class DataController : Controller
+    {
+        [HttpPost]
+        public ActionResult getLoginData(UserModel obj)
+        {
+            CarRentEntities db = new CarRentEntities();
+            var user = db.Users.FirstOrDefault(x => x.login.Equals(obj.Login) && x.password.Equals(obj.Password));
+            return new JsonResult { Data = user, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+    }
+}
